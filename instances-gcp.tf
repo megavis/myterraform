656ba1e3ef92f11bb62f786330b77c0d1bd47ed9
megavis <58000641+megavis@users.noreply.github.com>
2020-11-25 23:02:35 +0200

    added iteration

--- instances-gcp.tf
+++ instances-gcp.tf
@@ -17,3 +17,25 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 }
+
+resource "google_compute_instance" "vm_iterated_instance" {
+  for_each     = {
+    instance_a = "A"
+    instance_b = "B"
+  }
+  
+  name         = "gcp-demo-instance-${each.value}"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
