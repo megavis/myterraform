3f8bf76a4076040f6da904374c88dc5a9bcb7dbe
megavis <58000641+megavis@users.noreply.github.com>
2020-12-03 00:30:53 +0200

    add default VPC

---
+++ vpc.tf
@@ -0,0 +1,22 @@
+variable "gcp_config" {
+}
+
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "${var.gcp_config.project_id}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.gcp_config.project_id}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.0.0.0/24"
+
+}
+
+output "region" {
+  value       = var.gcp_config.region
+  description = "region"
+}
