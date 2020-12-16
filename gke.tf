c48def7372e958ac89d1f9fbe222e3b5568443ae
megavis <58000641+megavis@users.noreply.github.com>
2020-12-17 00:16:51 +0200

    changed machina type

--- gke.tf
+++ gke.tf
@@ -55,7 +55,7 @@ resource "google_container_node_pool" "primary_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "f1-micro"
+    machine_type = "e2-standard-2"
     tags         = ["gke-node", "${var.gcp_config.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
